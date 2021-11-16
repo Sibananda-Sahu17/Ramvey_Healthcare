5346c913d84b267435cbbdbea751570f65d28fc9
Manisa Basak <73214790+manisa18@users.noreply.github.com>
2021-11-16 22:24:40 +0530

    Created Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import './App.css';
+import Navbar from './Components/Navbar/Navbar.js';
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import Home from './Components/Home/Home.js';
+import Company from './Components/Company/Company.js';
+import Products from './Components/Products/Products.js';
+import RD from './Components/R&D/R&D.js';
+import Careers from './Components/Careers/Careers.js';
+import License from './Components/License/License.js';
+import Contact from './Components/Contact/Contact.js';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className="App">
+           <Navbar />
+
+        <div>
+          <Switch>
+
+            <Route path='/' exact>
+              <Home />
+            </Route>
+
+            <Route path='/company' exact>
+              <Company />
+            </Route>
+
+            <Route path='/products' exact>
+              <Products />
+            </Route>
+
+            <Route path='/rd' exact>
+              <RD />
+            </Route>
+
+            <Route path='/careers' exact>
+              <Careers />
+            </Route>
+
+            <Route path='/license' exact>
+              <License />
+            </Route>
+
+            <Route path='/contact' exact>
+              <Contact />
+            </Route>
+
+          </Switch>
+        </div>
+      </div>
+      </Router>
   );
 }
 
---
+++ src/Components/License/License.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            license
+        </div>
+    )
+}
+
+export default Footer
