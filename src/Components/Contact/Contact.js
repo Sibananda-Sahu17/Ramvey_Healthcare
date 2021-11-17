53fc4950673b524ea93ff6a5e2546b21a59bc0c8
Sibananda Sahu <sibananda6481@gmail.com>
2021-11-18 01:23:54 +0530

    create products and contact page partially

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import contact from '../Images/contact.jpeg'
 function Contact() {
     return (
         <div>
-            contact
+            <section>
+            <div className="flex justify-center items-center">
+                <h1 className="flex absolute tracking-widest text-white text-center font-bold text-base p-19 sm:font-bold sm:text-2xl md:font-bold md:text-3xl lg:font-extrabold lg:text-4xl xl:font-extrabold xl:text-5xl">We are proud of our reputation <br />for delivering the highest quality work</h1>
+                <img className="w-full h-auto" src={contact} />
+            </div>
+            </section>
+            <div className="">
+                
+            </div>
         </div>
     )
 }
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,11 +1,21 @@
 import React from 'react'
-
-function Footer() {
+import Product from '../Images/products.jpg'
+function Products() {
     return (
         <div>
-            products
+            <section>
+            <div className="flex justify-center items-center">
+                <h1 className="flex absolute tracking-widest text-white text-center font-bold text-base p-19 sm:font-bold sm:text-2xl md:font-bold md:text-3xl lg:font-extrabold lg:text-4xl xl:font-extrabold xl:text-5xl">We are proud of our reputation <br />for delivering the highest quality work</h1>
+                <img className="w-full h-auto " src={Product} />
+            </div>
+            </section>
+            <section>
+            <div className="text-center mb-16">
+                    <h1 className="text-2xl m-1 mt-3 font-bold sm:text-3xl sm:m-3 sm:mt-5 md:text-4xl md:m-5 md:mt-7 lg:text-5xl lg:m-7 lg:mt-5">Prescription Medicine</h1>
+                </div>
+            </section>
         </div>
     )
 }
 
-export default Footer
+export default Products
