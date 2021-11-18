ac1ae6c51593d23a133dd4cd37bfa3524d9039bf
Manisa Basak <73214790+manisa18@users.noreply.github.com>
2021-11-19 03:05:24 +0530

    contact page responsive

--- src/Components/Contact/AddressDetails.js
+++ src/Components/Contact/AddressDetails.js
@@ -1,28 +1,45 @@
 import React from 'react'
-
+import { FaMapSigns } from 'react-icons/fa';
+import { IoCall } from "react-icons/io5";
+import { GrMail } from "react-icons/gr";
 function AddressDetails() {
     return (
+      <div>
         <div>
-            <div>
-                <p>Please contact us using the information below.</p>
-              </div>
-
-              <div className="flex flex-col w-1/2">
-                <div className="flex flex-row flex-wrap items-center">
-                  <p className="mt-6">Registered Office</p>
-                  <p className="items-center flex justify-center">Mez &amp; First Floor, A-83, Okhla Phase - 2</p>
-                  <p>South Delhi, Delhi -110020 </p>
-                </div>
-
-                <div className="my-6">
-                  <p>+91-8948333866</p>
-                </div>
-                <div className="my-6">
-                  <p>info@kaymedpharma.com</p>
-                </div>
-              </div>
+          <p>Please contact us using the information below.</p>
         </div>
-    )
+
+        <div className="flex flex-col lg:w-1/2">
+          <div className="flex flex-wrap  flex-col lg:justify-start justify-center  ">
+            <p className="mt-6 flex font-bold lg:w-48 text-lg lg:justify-start justify-center">
+              <FaMapSigns className="mr-2 mb-2" size={20} />
+              Registered Office
+            </p>
+            <p className="flex justify-center">
+              Mez &amp; First Floor, A-83, Okhla Phase - 2
+            </p>
+            <p>South Delhi, Delhi -110020 </p>
+          </div>
+
+          <div>
+            <p className="my-6 flex text-lg lg:justify-start justify-center">
+              <IoCall className="mr-2 mb-2" size={20} />
+              +91-8948333866
+            </p>
+          </div>
+          <div>
+            <p className="my-6 flex text-lg lg:justify-start justify-center ">
+              <a
+                className=" flex text-lg  "
+                href="mailto: info@kaymedpharma.com">
+                <GrMail className="mr-2 mb-2" size={20} />
+                info@kaymedpharma.com
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default AddressDetails
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -5,7 +5,6 @@ import AddressDetails from "./AddressDetails";
 
 function Contact() {
   return (
-
     <div>
       <div className="">
         <div className="flex justify-center items-center">
@@ -13,19 +12,21 @@ function Contact() {
             We are proud of our reputation <br />
             for delivering the highest quality work
           </h1>
-          <img className="w-full h-auto"
-            src={contact}
-            alt="contact" />
+          <img className="w-full h-auto" src={contact} alt="contact" />
         </div>
 
-        <h1 className="text-center m-10 text-2xl m-1 mt-3 font-bold sm:text-3xl sm:m-3 sm:mt-5 md:text-4xl md:m-5 md:mt-7 lg:text-5xl lg:m-7 lg:mt-5">Kaymed Pharmaceuticals Pvt. Ltd.</h1>
-        <div className="flex justify-center items-center">
-          <div className="flex flex-col border-2 my-6 md:flex-row md:mx-6 md:my-0 w-8xl p-8 rounded-xl shadow-lg">
-            <div className="flex flex-col m-3 justify-between">
-              <AddressDetails />
-            </div>
-            <div className="flex w-1/2 justify-center">
-              <ContactForm />
+        <div className="my-16 mx-4">
+          <h1 className="text-center m-10 text-2xl mt-3 font-bold sm:text-3xl sm:m-3 sm:mt-5 md:text-4xl md:m-5 md:mt-7 lg:text-5xl lg:m-7 lg:mt-5">
+            Kaymed Pharmaceuticals Pvt. Ltd.
+          </h1>
+          <div className="flex overflow-hidden  justify-center items-center">
+            <div className="flex  lg:flex-row lg:justify-between flex-col border-2 my-6  md:mx-6 w-8xl px-32 py-16 rounded-xl shadow-lg">
+              <div className="flex justify-center lg:mr-16 my-8 lg:text-left text-center">
+                <AddressDetails />
+              </div>
+              <div className="flex lg:w-1/2 w-full lg:justify-end justify-center">
+                <ContactForm />
+              </div>
             </div>
           </div>
         </div>
