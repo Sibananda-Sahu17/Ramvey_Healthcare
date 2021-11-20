5bc13f2f2500b889caad57b5f129128d03e5a53c
Manisa Basak <73214790+manisa18@users.noreply.github.com>
2021-11-20 19:56:58 +0530

    ui changes

--- src/Components/Home/OurPolicyCarousel.js
+++ src/Components/Home/OurPolicyCarousel.js
@@ -1,60 +1,64 @@
-import React, { Component } from "react";
+import React from "react";
 import Slider from "react-slick";
 import { CarouselFiveDetails } from "./CorouselFiveDetails/CarouselFiveDetails";
 import '../../App.css';
-export default class MultipleItems extends Component {
-  render() {
-    const carouselFive = CarouselFiveDetails.map(
-      ({ id, image, heading, content }) => {
-        return (
-          <div key={id} className="hover:bg-black container">
-            <img className="2xl:w-full image imgHeight" src={image} alt={heading} />
+function OurPolicyCarousel() {
+  
+        const carouselFive = CarouselFiveDetails.map(
+          ({ id, image, heading, content }) => {
+            return (
+              <div key={id} className="hover:bg-black container">
+                <img
+                  className="2xl:w-full image imgHeight"
+                  src={image}
+                  alt={heading}
+                />
 
-            <div class="flex flex-col overlay cursor-pointer text-white justify-center items-center">              
-            <h1 className="text-xl uppercase text-white text-center font-bold">
-                  {heading}
-                </h1>
-                <h1 className=" text-md p-2 text-center text-white">
-                  {content}
-                </h1>
-            </div>
-          </div>
+                <div class="flex flex-col overlay cursor-pointer text-white justify-center items-center">
+                  <h1 className="text-xl uppercase text-white text-center font-bold">
+                    {heading}
+                  </h1>
+                  <h1 className=" text-md p-2 text-center text-white">
+                    {content}
+                  </h1>
+                </div>
+              </div>
+            );
+          }
         );
-      }
-    );
 
-    const settings = {
-      arrows: true,
-      infinite: true,
-      speed: 1000,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      responsive: [
-        {
-          breakpoint: 2000,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-          },
-        },
-        {
-          breakpoint: 1650,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-          },
-        },
-        {
-          breakpoint: 462,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          },
-        },
-      ],
-    };
+        const settings = {
+          arrows: true,
+          infinite: true,
+          speed: 1000,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 3000,
+          responsive: [
+            {
+              breakpoint: 2000,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+              },
+            },
+            {
+              breakpoint: 1650,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              },
+            },
+            {
+              breakpoint: 850,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              },
+            },
+          ],
+        };
     return (
       <div>
         <Slider className=" h-48 object-contain" {...settings}>
@@ -63,4 +67,4 @@ export default class MultipleItems extends Component {
       </div>
     );
   }
-}
+export default OurPolicyCarousel;
