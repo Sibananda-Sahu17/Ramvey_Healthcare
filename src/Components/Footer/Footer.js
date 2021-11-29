3dde2ab41ea907c123b38a665f685738e9ce75e9
Sibananda Sahu <sibananda6481@gmail.com>
2021-11-29 19:40:58 +0530

    fixed footer in products page

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="pt-10">
-      <div className="bg-black p-4">
+    <div className="pt-10 fixed inset-x-0 bottom-0">
+      <div className="bg-black p-4 ">
         <div className="text-center">
           <p className="text-white">
             Copyright 2021 Ramvey Healthcare Pvt Ltd. | All rights reserved{" "}
